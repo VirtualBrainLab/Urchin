de6ae56f4daf302f79a760075e44e258ac277b24
Dan Birman <danbirman@gmail.com>
2022-03-02 15:53:32 -0800

    Trying a different way of enabling CORS

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const socket = require("socket.io");
-const cors = require('cors');
+// const cors = require('cors');
 
 // App setup
 const port = process.env.PORT || 5000
@@ -9,9 +9,16 @@ const server = app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 // Static files
 app.use(express.static("public"));
-app.use(cors());
+// app.use(cors());
 
 // Socket setup
 const io = socket(server);
