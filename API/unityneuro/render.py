b37b22043a7f202bf34f7a610e071a0cdd5ba32b
Dan Birman <danbirman@gmail.com>
2022-10-08 12:52:03 -0700

    Updating to 0.4.3
    - Changing server target to /Urchin/

--- API/unityneuro/render.py
+++ API/unityneuro/render.py
@@ -61,7 +61,7 @@ def setup(verbose = True, localhost = False, standalone = False):
 		sio.connect('https://um-commserver.herokuapp.com/')
 
 	if not standalone:
-		url = "https://data.virtualbrainlab.org/UnityNeuro/?ID=" + ID
+		url = "https://data.virtualbrainlab.org/Urchin/?ID=" + ID
 		webbrowser.open(url)
 
 def close():
