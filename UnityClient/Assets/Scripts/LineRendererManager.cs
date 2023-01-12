03b03a7ad6ca7b8e05aecaf8b58fa4e37a10e7b4
Jasmine Schoch <jasmineyschoch@gmail.com>
2023-01-11 19:58:17 -0800

    Update LineRendererManager.cs

--- UnityClient/Assets/Scripts/LineRendererManager.cs
+++ UnityClient/Assets/Scripts/LineRendererManager.cs
@@ -16,20 +16,32 @@ public class LineRendererManager : MonoBehaviour
     public void CreateLine(List<string> lines)
     {
         //instantiating game object w line renderer component
+        foreach(string line in lines)
+        {
+            _lineRenderers.Add(line, new GameObject());
+            //in theory, creates new entry to the dictionary with the name of the line [line] and associates it with a new Game Object
+            _lineRenderers[line].AddComponent(LineRendererManager)
+            //adds the line renderer component to the line renderer manager (actually creates the line of the empty object)
+        }
 
     }
 
     public void DeleteLine(List<string> lines) { 
     
         //calls destroy (the one specific line)
+        foreach(string line in lines)
+        {
+            Destroy(_lineRenderers[line]);
+        }
     }
 
     public void SetLinePosition(Dictionary<string, List<Vector3>> linePositions)
     {
         //set line renderer position
+        _lineRenderers[linePositions.Keys].SetPosition(linePositions.Values);
     }
     public void SetLineColor(Dictionary<string, List<Color>> lineColors)
     {
-
+        _lineRenderers[lineColors.Keys].SetColors(lineColors.Values);
     }
 }
