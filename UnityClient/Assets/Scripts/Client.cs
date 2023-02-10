66b3514173e88e3d7fe7c7e2b73d1269349fb6e2
Dan Birman <danbirman@gmail.com>
2023-02-10 10:51:35 -0800

    Adding manager code

--- UnityClient/Assets/Scripts/Client.cs
+++ UnityClient/Assets/Scripts/Client.cs
@@ -9,23 +9,16 @@ using System.Threading.Tasks;
 /// <summary>
 /// Entry point for all client-side messages coming from the Python API
 /// Handles messages and tracks creation/destruction of prefab objects
-/// 
-/// TODO:
-///  - needs a refactor to separate neurons/probes/text/areas and put them in their own components
 /// </summary>
 public class Client : MonoBehaviour
 {
     [SerializeField] UM_Launch main;
     [SerializeField] CCFModelControl modelControl;
-    [SerializeField] BrainCameraController cameraControl;
     [SerializeField] private bool localhost;
 
     // UI
-    [SerializeField] private Canvas uiCanvas;
     [SerializeField] private TextMeshProUGUI idInput;
 
-    // VOLUMES
-    [SerializeField] private UM_VolumeRenderer volRenderer;
 
     // NEURONS
     [SerializeField] private GameObject neuronPrefab;
@@ -34,20 +27,16 @@ public class Client : MonoBehaviour
     [SerializeField] private Transform neuronParent;
     private Dictionary<string, GameObject> neurons;
 
-    // TEXT
-    [SerializeField] private GameObject textParent;
-    [SerializeField] private GameObject textPrefab;
-    private Dictionary<string, GameObject> texts;
 
     #region Managers
     [SerializeField] private LineRendererManager _lineRendererManager;
     [SerializeField] private PrimitiveMeshManager _primitiveMeshManager;
     [SerializeField] private ProbeManager _probeManager;
     [SerializeField] private AreaManager _areaManager;
+    [SerializeField] private TextManager _textManager;
+    [SerializeField] private VolumeManager _volumeManager;
+    [SerializeField] private CameraManager _cameraManager;
     #endregion
-    //Line Renderer
-
-    //Primitive Mesh Renderer
 
     // NODES
 
@@ -61,7 +50,10 @@ public class Client : MonoBehaviour
     private void Awake()
     {
         neurons = new Dictionary<string, GameObject>();
-        texts = new Dictionary<string, GameObject>();
+
+        if (_lineRendererManager == null || _primitiveMeshManager == null || _probeManager == null || _areaManager == null ||
+            _textManager == null || _volumeManager == null || _cameraManager == null)
+            throw new Exception("All managers must be linked in the editor!");
     }
 
     /// <summary>
@@ -76,9 +68,13 @@ public class Client : MonoBehaviour
         string url = "https://urchin-commserver.herokuapp.com/";
 #endif
         Debug.Log("Attempting to connect: " + url);
+
         manager = localhost ? new SocketManager(new Uri(url)) : new SocketManager(new Uri(url));
 
         manager.Socket.On("connect", Connected);
+#if UNITY_EDITOR
+        manager.Socket.On("reconnect", () => { Debug.Log("(Client) client reconnected -- could be sign of a timeout issue"); });
+#endif
 
         // CCF Areas
         manager.Socket.On<Dictionary<string, bool>>("SetAreaVisibility", _areaManager.SetAreaVisibility);
@@ -92,12 +88,12 @@ public class Client : MonoBehaviour
         manager.Socket.On<string>("LoadDefaultAreas", _areaManager.LoadDefaultAreas);
 
         // 3D Volumes
-        manager.Socket.On<List<object>>("SetVolumeVisibility", UpdateVolumeVisibility);
-        manager.Socket.On<List<object>>("SetVolumeDataMeta", UpdateVolumeMeta);
-        manager.Socket.On<byte[]>("SetVolumeData", UpdateVolumeData);
-        manager.Socket.On<string>("CreateVolume", CreateVolume);
-        manager.Socket.On<string>("DeleteVolume", DeleteVolume);
-        manager.Socket.On<List<string>>("SetVolumeColormap", SetVolumeColormap);
+        manager.Socket.On<List<object>>("SetVolumeVisibility", _volumeManager.SetVisibility);
+        manager.Socket.On<List<object>>("SetVolumeDataMeta", _volumeManager.SetMetadata);
+        manager.Socket.On<byte[]>("SetVolumeData", _volumeManager.SetData);
+        manager.Socket.On<string>("CreateVolume", _volumeManager.Create);
+        manager.Socket.On<string>("DeleteVolume", _volumeManager.Delete);
+        manager.Socket.On<List<string>>("SetVolumeColormap", _volumeManager.SetVolumeColormap);
 
         // Neurons
         manager.Socket.On<List<string>>("CreateNeurons", CreateNeurons);
@@ -118,21 +114,21 @@ public class Client : MonoBehaviour
         manager.Socket.On<Dictionary<string, List<float>>>("SetProbeSize", _probeManager.SetProbeScale);
 
         // Camera
-        manager.Socket.On<List<float>>("SetCameraTarget", SetCameraTarget);
-        manager.Socket.On<List<float>>("SetCameraPosition", SetCameraPosition);
-        manager.Socket.On<List<float>>("SetCameraRotation", SetCameraRotation);
-        manager.Socket.On<string>("SetCameraTargetArea", SetCameraTargetArea);
-        manager.Socket.On<float>("SetCameraZoom", SetCameraZoom);
-        manager.Socket.On<List<float>>("SetCameraPan", SetCameraPan);
+        manager.Socket.On<List<float>>("SetCameraTarget", _cameraManager.SetCameraTarget);
+        manager.Socket.On<List<float>>("SetCameraPosition", _cameraManager.SetCameraPosition);
+        manager.Socket.On<List<float>>("SetCameraRotation", _cameraManager.SetCameraRotation);
+        manager.Socket.On<string>("SetCameraTargetArea", _cameraManager.SetCameraTargetArea);
+        manager.Socket.On<float>("SetCameraZoom", _cameraManager.SetCameraZoom);
+        manager.Socket.On<List<float>>("SetCameraPan", _cameraManager.SetCameraPan);
 
 
         // Text
-        manager.Socket.On<List<string>>("CreateText", CreateText);
-        manager.Socket.On<List<string>>("DeleteText", DeleteText);
-        manager.Socket.On<Dictionary<string, string>>("SetTextText", SetText);
-        manager.Socket.On<Dictionary<string, string>>("SetTextColors", SetTextColors);
-        manager.Socket.On<Dictionary<string, int>>("SetTextSizes", SetTextSizes);
-        manager.Socket.On<Dictionary<string, List<float>>>("SetTextPositions", SetTextPositions);
+        manager.Socket.On<List<string>>("CreateText", _textManager.Create);
+        manager.Socket.On<List<string>>("DeleteText", _textManager.Delete);
+        manager.Socket.On<Dictionary<string, string>>("SetTextText", _textManager.SetText);
+        manager.Socket.On<Dictionary<string, string>>("SetTextColors", _textManager.SetColor);
+        manager.Socket.On<Dictionary<string, int>>("SetTextSizes", _textManager.SetSize);
+        manager.Socket.On<Dictionary<string, List<float>>>("SetTextPositions", _textManager.SetPosition);
 
         // Line Renderer
         manager.Socket.On<List<string>>("CreateLine", _lineRendererManager.CreateLine);
@@ -190,106 +186,6 @@ public class Client : MonoBehaviour
 #endif
     }
 
-    // VOLUME CONTROLS
-
-
-    private void UpdateVolumeVisibility(List<object> data)
-    {
-        if (((string)data[0]).Equals("allen"))
-            volRenderer.DisplayAllenVolume((bool)data[1]);
-        else
-            volRenderer.SetVolumeVisibility((string)data[0], (bool)data[1]);
-    }
-
-    private void SetVolumeColormap(List<string> obj)
-    {
-        string name = obj[0];
-        obj.RemoveAt(0);
-        volRenderer.SetVolumeColormap(name, obj);
-    }
-
-    private void CreateVolume(string name)
-    {
-        volRenderer.CreateVolume(name);
-    }
-
-    private void DeleteVolume(string name)
-    {
-        volRenderer.DeleteVolume(name);
-    }
-
-    private void UpdateVolumeMeta(List<object> data)
-    {
-        volRenderer.AddVolumeMeta((string)data[0], (int)data[1], (bool)data[2]);
-    }
-    private void UpdateVolumeData(byte[] bytes)
-    {
-        volRenderer.AddVolumeData(bytes);
-    }
-    //void OnFrame(Socket socket, Packet packet, params object[] args)
-    //{
-    //    texture.LoadImage(packet.Attachments[0]);
-    //}
-
-    // CAMERA CONTROLS
-
-
-    private void SetCameraRotation(List<float> obj)
-    {
-        cameraControl.SetBrainAxisAngles(new Vector3(obj[1], obj[0], obj[2]));
-    }
-
-    private void SetCameraZoom(float obj)
-    {
-        cameraControl.SetZoom(obj);
-    }
-
-    private void SetCameraPosition(List<float> obj)
-    {
-        // position in ml/ap/dv relative to ccf 0,0,0
-        LogError("Setting camera position not implemented yet. Use set_camera_target and set_camera_rotation instead.");
-        //Vector3 ccfPosition25 = new Vector3(obj[0]/25, obj[1]/25, obj[2]/25);
-        //cameraControl.SetOffsetPosition(Utils.apdvlr2World(ccfPosition25));
-    }
-
-    private void SetCameraYAngle(float obj)
-    {
-        cameraControl.SetSpin(obj);
-    }
-
-    private void SetCameraTargetArea(string obj)
-    {
-        (int ID, bool full, bool leftSide, bool rightSide) = _areaManager.GetID(obj);
-        CCFTreeNode node = modelControl.tree.findNode(ID);
-        if (node != null)
-        {
-            Vector3 center;
-            if (full)
-                center = node.GetMeshCenterFull();
-            else
-                center = node.GetMeshCenterSided(leftSide);
-            cameraControl.SetCameraTarget(center);
-        }
-        else
-            UM_Launch.Log("Failed to find node to set camera target: " + obj);
-    }
-
-    private void SetCameraTarget(List<float> mlapdv)
-    {
-        // data comes in in um units in ml/ap/dv
-        // note that (0,0,0) in world is the center of the brain
-        // so offset by (-6.6 ap, -4 dv, -5.7 lr) to get to the corner
-        // in world space, x = ML, y = DV, z = AP
-
-        Vector3 worldCoords = new Vector3(5.7f - mlapdv[0]/1000f, 4f - mlapdv[2] / 1000f, mlapdv[1] / 1000f - 6.6f);
-        cameraControl.SetCameraTarget(worldCoords);
-    }
-
-    private void SetCameraPan(List<float> panXY)
-    {
-        cameraControl.SetCameraPan(new Vector2(panXY[0], panXY[1]));
-    }
-
 #region Clear
 
     private void Clear(string val)
@@ -300,8 +196,8 @@ public class Client : MonoBehaviour
                 ClearNeurons();
                 _probeManager.ClearProbes();
                 _areaManager.ClearAreas();
-                ClearVolumes();
-                ClearTexts();
+                _volumeManager.Clear();
+                _textManager.Clear();
                 break;
             case "neurons":
                 ClearNeurons();
@@ -313,10 +209,10 @@ public class Client : MonoBehaviour
                 _areaManager.ClearAreas();
                 break;
             case "volumes":
-                ClearVolumes();
+                _volumeManager.Clear();
                 break;
             case "texts":
-                ClearTexts();
+                _textManager.Clear();
                 break;
         }
     }
@@ -329,31 +225,8 @@ public class Client : MonoBehaviour
         neurons = new Dictionary<string, GameObject>();
     }
 
-    private void ClearVolumes()
-    {
-        Debug.Log("(Client) Clearing volumes");
-        volRenderer.Clear();
-    }
-
-    private void ClearTexts()
-    {
-        Debug.Log("(Client) Clearing text");
-        foreach (GameObject text in texts.Values)
-            Destroy(text);
-        texts = new Dictionary<string, GameObject>();
-    }
-
 #endregion
 
-    private void SetVolumeAnnotationColor(Dictionary<string, string> data)
-    {
-        UM_Launch.Log("Not implemented");
-    }
-
-    private void SetVolumeSlice(List<float> obj)
-    {
-        UM_Launch.Log("Not implemented");
-    }
 
     // NEURONS
 
@@ -426,90 +299,6 @@ public class Client : MonoBehaviour
             neurons.Remove(id);
         }
     }
-
-#region Text
-
-    private void CreateText(List<string> data)
-    {
-        Debug.Log("Creating text");
-        foreach (string textName in data)
-        {
-            if (!texts.ContainsKey(textName))
-            {
-                GameObject textGO = Instantiate(textPrefab, textParent.transform);
-                texts.Add(textName, textGO);
-            }
-        }
-    }
-
-    private void DeleteText(List<string> data)
-    {
-        Debug.Log("Deleting text");
-        foreach (string textName in data)
-        {
-            if (texts.ContainsKey(textName))
-            {
-                Destroy(texts[textName]);
-                texts.Remove(textName);
-            }
-        }
-    }
-
-    private void SetText(Dictionary<string, string> data)
-    {
-        foreach (KeyValuePair<string, string> kvp in data)
-        {
-            if (texts.ContainsKey(kvp.Key))
-                texts[kvp.Key].GetComponent<TMP_Text>().text = kvp.Value;
-        }
-    }
-
-    private void SetTextColors(Dictionary<string, string> data)
-    {
-        Debug.Log("Setting text colors");
-        foreach (KeyValuePair<string, string> kvp in data)
-        {
-            Color newColor;
-            if (texts.ContainsKey(kvp.Key) && ColorUtility.TryParseHtmlString(kvp.Value, out newColor))
-            {
-                texts[kvp.Key].GetComponent<TMP_Text>().color = newColor;
-            }
-            else
-                LogError("Failed to set text color to: " + kvp.Value);
-        }
-    }
-
-    private void SetTextSizes(Dictionary<string, int> data)
-    {
-        Debug.Log("Setting text sizes");
-        foreach (KeyValuePair<string, int> kvp in data)
-        {
-            if (texts.ContainsKey(kvp.Key))
-                texts[kvp.Key].GetComponent<TMP_Text>().fontSize = kvp.Value;
-        }
-    }
-
-    private void SetTextPositions(Dictionary<string, List<float>> data)
-    {
-#if UNITY_EDITOR
-        Debug.Log("Setting text positions");
-#endif
-        Vector2 canvasWH = new Vector2(uiCanvas.GetComponent<RectTransform>().rect.width, uiCanvas.GetComponent<RectTransform>().rect.height);
-        foreach (KeyValuePair<string, List<float>> kvp in data)
-        {
-            if (texts.ContainsKey(kvp.Key))
-            {
-                texts[kvp.Key].transform.localPosition = new Vector2(canvasWH.x * kvp.Value[0] / 2, canvasWH.y * kvp.Value[1] / 2);
-            }
-            else
-            {
-                Debug.Log("Couldn't set position for " + kvp.Key);
-                LogError(string.Format("Couldn't set position of {0}", kvp.Key));
-            }
-        }
-    } 
-     
-#endregion
 
     ////
     //// SOCKET FUNCTIONS
