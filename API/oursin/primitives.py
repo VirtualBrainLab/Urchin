d3012527e84940da3ad41d3c1d423c6988be255a
Jasmine Schoch <jasmineyschoch@gmail.com>
2023-03-01 15:06:44 -0800

    reformatted primitives API

--- API/oursin/primitives.py
+++ API/oursin/primitives.py
@@ -1,130 +1,169 @@
 """Primitive meshes"""
 
 from . import client
-
+counter = 0
   ## Primitive Mesh Renderer
-class Primitives:
+class Primitive:
   #Run everytime an object is created, sets the fields to defaults if one is not given, and sends the info to Unity
-  def __init__(self,position, scale, color, material):
-    client.sio.emit('CreateMesh', self)
+  #id = int index counter
+  def __init__(self,position, scale, color, material, id):
+    counter +=1
+    self.id = counter
+    client.sio.emit('CreateMesh', self.id)
+
     if(position == None):
        position = [0,0,0]
     self.position = position
-    client.sio.emit('SetPosition', {self: position})
+    client.sio.emit('SetPosition', {self.id: position})
       
     if(scale == None):
       scale = [1,1,1]
     self.scale = scale
-    client.sio.emit('SetScale', {self: scale})
+    client.sio.emit('SetScale', {self.id: scale})
       
     if(color == None):
       color = '#FFFFFF'
     self.color = color
-    client.sio.emit('SetColor',{self: color})
+    client.sio.emit('SetColor',{self.id: color})
 
-    if(material == None):
-       material = 'default'
+  def delete(self):
+    client.sio.emit('DeleteMesh', self.id)
+  
+  def set_position(self, position):
+    self.position = position
+    client.sio.emit('SetPosition', {self.id: position})
+  
+  def set_scale(self, scale):
+    self.scale = scale
+    client.sio.emit('SetScale', {self.id: scale})
+  
+  def set_color(self, color):
+    self.color = color
+    client.sio.emit('SetColor',{self.id: color})
+
+  def set_material(self, material):
     self.material = material
-    client.sio.emit('SetMaterial',{self: material})  
+    client.sio.emit('SetMaterial',{self.id: material})
 
-  #actually initializes each object(s), does use any parameters other than how many to initialize (uses all defaults)
-  def create(numObjects):
-    if (numObjects == 1)
-      mesh = Primitives()
-      client.sio.emit('CreateMesh', mesh)
-    else:
-      mesh_names = []
-      for(int i=1; i<= numObjects; i++)
-        mesh_names[i] = Primitives()
-      client.sio.emit('CreateMesh', mesh_names)
 
+
+#actually initializes each object(s), doesn't use any parameters other than how many to initialize (uses all defaults)
+def create(numObjects):
+  prim_names = []
+  for i in range(0, numObjects):
+    prim_names[i] = Primitive()
+  return(prim_names)
+
+def delete(meshes_list):
+  mesh_ids = [x.id for x in meshes_list]
+  client.sio.emit('DeleteMesh', mesh_ids)
+
+def set_positions(meshes_list, positions_list):
+  mesh_ids = [x.id for x in meshes_list]
+  mesh_pos = {mesh_ids,positions_list}
+  client.sio.emit('SetPosition', mesh_pos)
+
+def set_scales(meshes_list, scales_list):
+  mesh_ids = [x.id for x in meshes_list]
+  mesh_scale = {mesh_ids,scales_list}
+  client.sio.emit('SetScale', mesh_scale)
+
+def set_colors(meshes_list, colors_list):
+  mesh_ids = [x.id for x in meshes_list]
+  mesh_colors = {mesh_ids, colors_list}
+  client.sio.emit('SetColor', mesh_colors)
+
+def set_material(meshes_list, materials_list):
+  mesh_ids = [x.id for x in meshes_list]
+  mesh_materials = {mesh_ids,materials_list}
+  client.sio.emit('SetMaterial', mesh_materials) 
 
 ##OLD CODE BELOW
     
     
 
-def create(mesh_names):
-	"""Creates primitive mesh
+# def create(mesh_names):
+# 	"""Creates primitive mesh
 
-  Parameters
-  ----------
-  mesh_names : list of strings
-	IDs of meshes being created
+#   Parameters
+#   ----------
+#   mesh_names : list of strings
+# 	IDs of meshes being created
       
-	Examples
-	--------
-	>>> urn.create(['cube1','cube2'])
-  """
-	client.sio.emit('CreateMesh', mesh_names)
+# 	Examples
+# 	--------
+# 	>>> urn.create(['cube1','cube2'])
+#   """
+# 	client.sio.emit('CreateMesh', mesh_names)
 
-def delete(mesh_names):
-	"""Deletes meshes
+# def delete(mesh_names):
+# 	"""Deletes meshes
 
-  Parameters
-  ----------
-  mesh_names : list of strings
-	IDs of meshes being deleted
+#   Parameters
+#   ----------
+#   mesh_names : list of strings
+# 	IDs of meshes being deleted
       
-	Examples
-	--------
-	>>> urn.delete(['cube1'])
-  """
-	client.sio.emit('DeleteMesh', mesh_names)
+# 	Examples
+# 	--------
+# 	>>> urn.delete(['cube1'])
+#   """
+# 	client.sio.emit('DeleteMesh', mesh_names)
 
-def set_position(mesh_pos):
-  """Set the position of mesh renderer
+# def set_position(mesh_pos):
+#   """Set the position of mesh renderer
 
-  Parameters
-  ----------
-  mesh_pos : dict {string : list of three floats}
-      dictionary of IDs and vertex positions of the mesh
+#   Parameters
+#   ----------
+#   mesh_pos : dict {string : list of three floats}
+#       dictionary of IDs and vertex positions of the mesh
       
-	Examples
-	--------
-	>>> urn.set_position({'cube1': [1, 2, 3]})
-  """
-  client.sio.emit('SetPosition', mesh_pos)
+# 	Examples
+# 	--------
+# 	>>> urn.set_position({'cube1': [1, 2, 3]})
+#   """
+#   client.sio.emit('SetPosition', mesh_pos)
 
-def set_scale(mesh_scale):
-  """Set the scale of mesh renderer
+# def set_scale(mesh_scale):
+#   """Set the scale of mesh renderer
 
-  Parameters
-  ----------
-  mesh_scale : dict {string : list of three floats}
-      dictionary of IDs and new scale of mesh
+#   Parameters
+#   ----------
+#   mesh_scale : dict {string : list of three floats}
+#       dictionary of IDs and new scale of mesh
       
-	Examples
-	--------
-	>>> urn.set_scale({'cube1': [3, 3, 3]})
-  """
-  client.sio.emit('SetScale', mesh_scale)
+# 	Examples
+# 	--------
+# 	>>> urn.set_scale({'cube1': [3, 3, 3]})
+#   """
+#   client.sio.emit('SetScale', mesh_scale)
 
-def set_color(mesh_color):
-  """Set the color of mesh renderer
+# def set_color(mesh_color):
+#   """Set the color of mesh renderer
 
-  Parameters
-  ----------
-  mesh_color : dict {string : string hex color}
-      dictionary of IDs and new hex color of mesh
+#   Parameters
+#   ----------
+#   mesh_color : dict {string : string hex color}
+#       dictionary of IDs and new hex color of mesh
       
-	Examples
-	--------
-	>>> urn.set_color({'cube1': '#FFFFFF'})
+# 	Examples
+# 	--------
+# 	>>> urn.set_color({'cube1': '#FFFFFF'})
 	
-  """
-  client.sio.emit('SetColor', mesh_color)
+#   """
+#   client.sio.emit('SetColor', mesh_color)
 
-def set_material(mesh_material):
-  """Set the material of mesh renderer
+# def set_material(mesh_material):
+#   """Set the material of mesh renderer
 
-  Parameters
-  ----------
-  mesh_material : dict {string : string}
-      dictionary of object IDs and name of new material
+#   Parameters
+#   ----------
+#   mesh_material : dict {string : string}
+#       dictionary of object IDs and name of new material
       
-	Examples
-	--------
-	>>> urn.set_material({'cube1': 'unlit'})
+# 	Examples
+# 	--------
+# 	>>> urn.set_material({'cube1': 'unlit'})
 	
-  """
-  client.sio.emit('SetMaterial', mesh_material)  
+#   """
+#   client.sio.emit('SetMaterial', mesh_material)  
