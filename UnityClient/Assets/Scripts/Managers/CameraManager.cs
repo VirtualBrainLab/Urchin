50d8129074140ebd4c13e78e9032336f2e8bc65d
Jasmine Schoch <112601674+jasmine-schoch@users.noreply.github.com>
2023-06-27 16:46:42 -0700

    Getting create function working on Unity end

--- UnityClient/Assets/Scripts/Managers/CameraManager.cs
+++ UnityClient/Assets/Scripts/Managers/CameraManager.cs
@@ -1,3 +1,4 @@
+using BestHTTP.SecureProtocol.Org.BouncyCastle.Utilities.IO.Pem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,20 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     //Keep a dictionary that maps string names to camera components 
-    private Dictionary<string, Camera> _cameras;
+    private Dictionary<string, Camera> _cameras = new(); //directly access the camera nested within the prefab
     [SerializeField] private GameObject _cameraPrefab;
     [SerializeField] private RenderTexture output1;
     [SerializeField] private RenderTexture output2;
     [SerializeField] private RenderTexture output3;
     [SerializeField] private RenderTexture output4;
 
-
+    public BrainCameraController cameraController;
 
     Stack<RenderTexture> textures = new();
+
+    List<string> cameraTest = new();
+    List<string> cameraTest2 = new();
+
 
     #region Unity functions
     private void Awake()
@@ -23,10 +28,30 @@ public class CameraManager : MonoBehaviour
         textures.Push(output3);
         textures.Push(output2);
         textures.Push(output1);
+
+        cameraTest.Add("1");
+        cameraTest.Add("2");
     }
+    private void Start()
+    {
+        CreateCamera(cameraTest);
+        float delayInSeconds = 2.0f; // Delay time in seconds
+
+        // Invoke a function after the specified delay
+        Invoke("CreateTest", delayInSeconds);
+        //Invoke("DeleteTest", delayInSeconds);
+    }
+
     #endregion
 
-
+    private void CreateTest()
+    {
+        CreateCamera(cameraTest2);
+    }
+    private void DeleteTest()
+    {
+        DeleteCamera(cameraTest);
+    }
 
 
 
@@ -39,11 +64,27 @@ public class CameraManager : MonoBehaviour
         {
             GameObject tempObject = Instantiate(_cameraPrefab);
             tempObject.name = $"camera_{camera}";
-            Camera cameraComponent = tempObject.GetComponent<Camera>();
-            _cameras.Add(camera, cameraComponent);
-            //in theory, creates new entry to the dictionary with the name of the camera [camera] and associates it with a new Game Object
-            //adds the camera component to the camera manager (actually creates the camera of the empty object)
-            cameraComponent.targetTexture = textures.Pop();
+            //cameraController.UserControllable = false; //will need to be changed if a new main camera is being created CAUSES TARGET TEXTURE TO NOT WORK
+
+            // Get all Camera components attached to children of the script's GameObject (since there are multiple)
+            Camera[] cameraComponenents = tempObject.GetComponentsInChildren<Camera>();
+
+            // Access and manipulate the cameras as needed
+            foreach (Camera cameraComponent in cameraComponenents)
+            {
+                // Do something with the camera
+                bool isCameraOn = cameraComponent.enabled;
+
+                // Use the boolean value as needed
+                if (isCameraOn)
+                {
+                    // Camera is turned on + connected to proper texture
+                    cameraComponent.targetTexture = textures.Pop();
+                    _cameras.Add(camera, cameraComponent); //doesn't take care of the whole prefab, but makes everything easier regarding camera manipulation?
+                                                 //in theory, creates new entry to the dictionary with the name of the camera [camera] and associates it with a new Game Object
+
+                }
+            }
         }
 
     }
@@ -54,8 +95,11 @@ public class CameraManager : MonoBehaviour
         //calls destroy (the one specific camera)
         foreach (string camera in cameras)
         {
-            Camera cameraComponent = _cameras[camera].gameObject.GetComponent<Camera>();
-            textures.Push(cameraComponent.targetTexture);
+            textures.Push(_cameras[camera].targetTexture);
+            //Camera cameraComponent = _cameras[camera].gameObject.GetComponent<Camera>();
+            //textures.Push(cameraComponent.targetTexture);
+            Transform parentObj = _cameras[camera].gameObject.transform.parent;
+            //Destroy(parentObj.gameObject);
             Destroy(_cameras[camera].gameObject);
             _cameras.Remove(camera);
         }
