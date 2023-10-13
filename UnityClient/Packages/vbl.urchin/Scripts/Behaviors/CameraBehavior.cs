a00c9421e5e6f92c5908c749a161251a84b03b91
Dan Birman <danbirman@gmail.com>
2023-10-13 15:52:27 -0700

    chore: removing references to UnityEditor from runtime scripts

--- UnityClient/Packages/vbl.urchin/Scripts/Behaviors/CameraBehavior.cs
+++ UnityClient/Packages/vbl.urchin/Scripts/Behaviors/CameraBehavior.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using Urchin.Cameras;
 using BrainAtlas;
 using Urchin.API;
 
