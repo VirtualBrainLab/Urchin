239814c3abbddc6cf6f123574b7775752aa9c620
Dan Birman <danbirman@gmail.com>
2023-10-18 13:52:02 -0700

    chore: minor

--- UnityClient/Packages/vbl.urchin/Scripts/Managers/CustomMeshManager.cs
+++ UnityClient/Packages/vbl.urchin/Scripts/Managers/CustomMeshManager.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomMeshManager : MonoBehaviour
+namespace Urchin.Managers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CustomMeshManager : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
