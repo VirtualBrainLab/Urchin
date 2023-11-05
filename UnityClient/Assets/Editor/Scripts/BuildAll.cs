d8a589fd0c2aaff827c1e7b552c82f3bc2bb8502
Dan Birman <danbirman@gmail.com>
2023-11-04 20:36:30 -0700

    feat: adding BuildAll script

---
+++ UnityClient/Assets/Editor/Scripts/BuildAll.cs
@@ -0,0 +1,80 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+public class BuildAll
+{
+    [MenuItem("Tools/Build All")]
+    public static void Build_All()
+    {
+        BuildWindows();
+        BuildLinux();
+        BuildWebGL();
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+    }
+
+    private static void BuildWindows()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/UnityMouseRenderer.unity" },
+            locationPathName = "Builds/Win64/Urchin.exe",
+            target = BuildTarget.StandaloneWindows64,
+            options = BuildOptions.None
+        };
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        SummarizeReport(report);
+    }
+
+    private static void BuildLinux()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/UnityMouseRenderer.unity" },
+            locationPathName = "Builds/Linux/Urchin",
+            target = BuildTarget.StandaloneLinux64,
+            options = BuildOptions.None
+        };
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux64);
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        SummarizeReport(report);
+    }
+
+    private static void BuildWebGL()
+    {
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/UnityMouseRenderer.unity" },
+            locationPathName = "Builds/WebGL/Urchin",
+            target = BuildTarget.WebGL,
+            options = BuildOptions.None
+        };
+
+        if (EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL))
+        {
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            SummarizeReport(report);
+        }
+        else
+            Debug.LogError("Failed to switch contexts?");
+    }
+
+    private static void SummarizeReport(BuildReport report)
+    {
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
